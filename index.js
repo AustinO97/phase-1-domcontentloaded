885972b7ad224c6cd439e0bd0e8bd4e314739269
Austin Oblow <austinoblow97@gmail.com>
2023-08-07 10:56:58 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// Your code goes heredocument.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let input = document.getElementById('text')
+    input.textContent = "This is really cool!"
+});
+      
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
